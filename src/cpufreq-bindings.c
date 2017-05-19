0ec6821c80fcceab18a2a0556e8d3735fdf0ce3b
Connor Imes <connor.k.imes@gmail.com>
2017-05-18 23:40:55 -0500

    Fix more warnings by not using VLAs. Test stricter compile flags in Travis CI. Code optimizations and reuse.

--- src/cpufreq-bindings.c
+++ src/cpufreq-bindings.c
@@ -55,7 +55,7 @@ static void conditional_close(int cond, int fd) {
   errno = err_save;
 }
 
-static ssize_t read_file_str(int fd, uint32_t core, char* buf, size_t len, const char* name) {
+static ssize_t read_file_by_fd_or_name(int fd, uint32_t core, char* buf, size_t len, const char* name, int trim) {
   ssize_t ret;
   int local_fd = fd <= 0;
   if (local_fd) {
@@ -63,16 +63,21 @@ static ssize_t read_file_str(int fd, uint32_t core, char* buf, size_t len, const
       return -1;
     }
   }
-  if ((ret = pread(fd, buf, len, 0)) < 0) {
-    PERROR(ERROR, "read_file_str: pread");
+  ret = pread(fd, buf, len, 0);
+  if (ret <= 0) {
+    if (ret == 0) {
+      errno = ENODATA;
+    }
+    PERROR(ERROR, "read_file_by_fd_or_name: pread");
+  } else if (trim) {
+    // strip newline character
+    buf[strcspn(buf, "\n")] = '\0';
   }
   conditional_close(local_fd, fd);
-  // strip newline character
-  buf[strcspn(buf, "\n")] = '\0';
   return ret;
 }
 
-static ssize_t write_file_str(int fd, uint32_t core, const char* buf, size_t len, const char* name) {
+static ssize_t write_file_by_fd_or_name(int fd, uint32_t core, const char* buf, size_t len, const char* name) {
   ssize_t ret;
   int local_fd = fd <= 0;
   if (local_fd) {
@@ -81,84 +86,59 @@ static ssize_t write_file_str(int fd, uint32_t core, const char* buf, size_t len
     }
   }
   if ((ret = pwrite(fd, buf, len, 0)) < 0) {
-    PERROR(ERROR, "write_file_str: pwrite");
+    PERROR(ERROR, "write_file_by_fd_or_name: pwrite");
   }
   conditional_close(local_fd, fd);
   return ret;
 }
 
 static uint32_t read_file_u32arr(int fd, uint32_t core, uint32_t* arr, uint32_t len, const char* name) {
+  char* tok;
+  char* ptr = NULL;
   uint32_t i = 0;
   // support string values for every core + a whitespace char + a terminating NULL char
   size_t buflen = len * (U32_MAX_LEN + 1) + 1;
-  char buf[buflen];
-  char* tok;
-  char* ptr = NULL;
-  int local_fd = fd <= 0;
-  int pret;
-  if (local_fd) {
-    if ((fd = cpufreq_bindings_open_file(name, core, O_RDONLY)) <= 0) {
-      return 0;
-    }
-  }
-  pret = pread(fd, buf, buflen, 0);
-  if (pret < 0) {
-    PERROR(ERROR, "read_file_u32arr: pread");
-  } else if (pret > 0) {
-    if (len == 1) {
+  char* buf = calloc(1, buflen);
+  if (buf != NULL && read_file_by_fd_or_name(fd, core, buf, buflen, name, 0) > 0) {
+    // tokenize and write to the array
+    tok = strtok_r(buf, " ", &ptr);
+    for (; tok != NULL && i < len; tok = strtok_r(NULL, " ", &ptr), i++) {
       errno = 0;
-      // optimization - work directly on the buffer rather than tokenizing
-      arr[i] = strtoul(buf, NULL, 0);
+      arr[i] = strtoul(tok, NULL, 0);
       if (errno) {
         PERROR(ERROR, "read_file_u32arr: strtoul");
-      } else {
-        i++;
-      }
-    } else {
-      // tokenize and write to the array
-      tok = strtok_r(buf, " ", &ptr);
-      for (; tok != NULL && i < len; tok = strtok_r(NULL, " ", &ptr), i++) {
-        errno = 0;
-        arr[i] = strtoul(tok, NULL, 0);
-        if (errno) {
-          PERROR(ERROR, "read_file_u32arr: strtoul");
-          i = 0;
-          tok = NULL;
-          break;
-        }
-      }
-      if (tok != NULL) {
-        // the array wasn't big enough
         i = 0;
-        errno = ERANGE;
+        tok = NULL;
+        break;
       }
     }
+    if (tok != NULL) {
+      // the array wasn't big enough
+      i = 0;
+      errno = ERANGE;
+    }
   }
-  conditional_close(local_fd, fd);
+  free(buf);
   return i;
 }
 
 static uint32_t read_file_u32(int fd, uint32_t core, const char* name) {
   uint32_t ret = 0;
-  read_file_u32arr(fd, core, &ret, 1, name);
+  char buf[U32_MAX_LEN];
+  if (read_file_by_fd_or_name(fd, core, buf, sizeof(buf), name, 0) > 0) {
+    errno = 0;
+    ret = strtoul(buf, NULL, 0);
+    if (errno) {
+      PERROR(ERROR, "read_file_u32: strtoul");
+    }
+  }
   return ret;
 }
 
 static ssize_t write_file_u32(int fd, uint32_t core, uint32_t val, const char* name) {
   char buf[U32_MAX_LEN];
-  ssize_t ret;
-  int local_fd = fd <= 0;
-  if (local_fd) {
-    if ((fd = cpufreq_bindings_open_file(name, core, O_RDWR)) <= 0) {
-      return -1;
-    }
-  }
   snprintf(buf, sizeof(buf), "%"PRIu32, val);
-  if ((ret = pwrite(fd, buf, sizeof(buf), 0)) < 0) {
-    PERROR(ERROR, "write_file_u32: pwrite");
-  }
-  conditional_close(local_fd, fd);
-  return ret;
+  return write_file_by_fd_or_name(fd, core, buf, sizeof(buf), name);
 }
 
 static const char* cpufreq_bindings_file_to_name(cpufreq_bindings_file file) {
@@ -261,22 +241,24 @@ uint32_t cpufreq_bindings_get_scaling_available_frequencies(int fd, uint32_t cor
 }
 
 uint32_t cpufreq_bindings_get_scaling_available_governors(int fd, uint32_t core, char* governors, size_t len, size_t width) {
-  char buf[len * GOVERNOR_NAME_MAX_LEN];
   char* ptr;
   char* tok;
   uint32_t i = 0;
-  if (read_file_str(fd, core, buf, sizeof(buf), FILE_SCALING_AVAILABLE_GOVERNORS) <= 0) {
-    return 0;
+  // support string values for every governor + a whitespace char + a terminating NULL char
+  size_t buflen = len * (GOVERNOR_NAME_MAX_LEN + 1) + 1;
+  char* buf = calloc(1, buflen);
+  if (buf != NULL && read_file_by_fd_or_name(fd, core, buf, buflen, FILE_SCALING_AVAILABLE_GOVERNORS, 1) > 0) {
+    tok = strtok_r(buf, " ", &ptr);
+    for (; tok != NULL && i < len; tok = strtok_r(NULL, " ", &ptr), i++) {
+      strncpy(&governors[i * width], tok, width);
+    }
+    if (tok != NULL) {
+      // the array wasn't big enough
+      i = 0;
+      errno = ERANGE;
+    }
   }
-  tok = strtok_r(buf, " ", &ptr);
-  for (; tok != NULL && i < len; tok = strtok_r(NULL, " ", &ptr), i++) {
-    strncpy(&governors[i * width], tok, width);
-  }
-  if (tok != NULL) {
-    // the array wasn't big enough
-    i = 0;
-    errno = ERANGE;
-  }
+  free(buf);
   return i;
 }
 
@@ -285,15 +267,15 @@ uint32_t cpufreq_bindings_get_scaling_cur_freq(int fd, uint32_t core) {
 }
 
 ssize_t cpufreq_bindings_get_scaling_driver(int fd, uint32_t core, char* driver, size_t len) {
-  return read_file_str(fd, core, driver, len, FILE_SCALING_DRIVER);
+  return read_file_by_fd_or_name(fd, core, driver, len, FILE_SCALING_DRIVER, 1);
 }
 
 ssize_t cpufreq_bindings_get_scaling_governor(int fd, uint32_t core, char* governor, size_t len) {
-  return read_file_str(fd, core, governor, len, FILE_SCALING_GOVERNOR);
+  return read_file_by_fd_or_name(fd, core, governor, len, FILE_SCALING_GOVERNOR, 1);
 }
 
 ssize_t cpufreq_bindings_set_scaling_governor(int fd, uint32_t core, const char* governor, size_t len) {
-  return write_file_str(fd, core, governor, len, FILE_SCALING_GOVERNOR);
+  return write_file_by_fd_or_name(fd, core, governor, len, FILE_SCALING_GOVERNOR);
 }
 
 uint32_t cpufreq_bindings_get_scaling_max_freq(int fd, uint32_t core) {
